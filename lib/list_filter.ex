833e0b82598c4051d194e475d9bde431431a88bb
Vinocis <vinicius.alexandre68@gmail.com>
2022-01-10 17:59:17 -0300

    first time using enum and anon functions

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,20 +1,10 @@
 defmodule ListFilter do
-
-  def odd_count(list), do: odd_count(list, 0)
-
-  defp odd_count([], acc), do: acc
-
-  defp odd_count([head | tail], acc) do
-    case is_odd?(head) do
-      true -> odd_count(tail, acc + 1)
-      false -> odd_count(tail, acc)
-    end
-  end
-
-  defp is_odd?(value) do
-    case Integer.parse(value) do
-      {int, _} -> rem(int, 2) != 0
-      :error -> false
-    end
+  def odd_count(list) do
+    Enum.reduce(list, 0, fn elem, acc ->
+      case Integer.parse(elem) do
+        {int, _} when rem(int, 2) == 1 -> acc + 1
+        _ -> acc
+      end
+    end)
   end
 end
